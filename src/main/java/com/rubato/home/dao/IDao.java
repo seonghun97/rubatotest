66e464c01357b8b320ef8e9f35947d4a3c01c8ef
ici <ici@DESKTOP-R9TEVC2>
2023-05-17 12:49:48 +0900

    param 에 #을 안씀

--- src/main/java/com/rubato/home/dao/IDao.java
+++ src/main/java/com/rubato/home/dao/IDao.java
@@ -1,6 +1,12 @@
 package com.rubato.home.dao;
 
+import java.util.List;
+
+import com.rubato.home.dto.RfboardDto;
+
 public interface IDao {
 	
 	public int boardWriteDao(String bname, String btitle, String bcontent, String buserid, int filecount);
+	public List<RfboardDto> boardListDao(); //게시판 모든글 리스트
+	public int boardTotalCountDao(); //게시판 총 게시글 개수
 }
